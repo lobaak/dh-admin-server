d4d21a0d1a68796d09c9714b49245a5713bb370a
Nathan Thorpe <nath.thorpe@gmail.com>
2020-07-16 17:30:19 +1000

    Add id and date to items

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const { v4: uuidv4 } = require("uuid");
 
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.id = uuidv4();
+    req.body.createdAt = Date.now();
+  }
+
+  next();
+});
 server.use(middlewares);
 server.use(router);
+
 server.listen(6996, () => {
   console.log("JSON Server is running");
 });
