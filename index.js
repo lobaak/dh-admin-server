862545005eaa6dfb785c444527535f1f655c72a0
Nathan Thorpe <nath.thorpe@gmail.com>
2020-07-17 16:12:28 +1000

    Update config and router

--- index.js
+++ index.js
@@ -4,13 +4,32 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const { v4: uuidv4 } = require("uuid");
 
+//github.com/typicode/json-server/issues/366#issuecomment-439008812
+https: router.db._.mixin({
+  getById(collection, id) {
+    const idProp = this.__id();
+    if (Array.isArray(id)) {
+      const ids = id.map((_id) => _id.toString());
+      return this.filter(collection, (doc) => {
+        if (this.has(doc, idProp)) {
+          return ids.includes(doc[idProp].toString());
+        }
+      });
+    }
+    return this.find(collection, (doc) => {
+      if (this.has(doc, idProp)) {
+        return doc[idProp].toString() === id.toString();
+      }
+    });
+  },
+});
+
 server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
   if (req.method === "POST") {
     req.body.id = uuidv4();
     req.body.createdAt = Date.now();
   }
-
   next();
 });
 server.use(middlewares);
