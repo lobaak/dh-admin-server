308ca33b0a8ae870460998795aa0dca7285a386d
Nathan Thorpe <nath.thorpe@gmail.com>
2020-07-22 15:12:41 +1000

    Use temp dir for db

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+const { join } = require("path");
+const { tmpdir } = require("os");
+const fs = require("fs");
+
 const jsonServer = require("json-server");
+const file = "db.json";
+const source = join(tmpdir(), file);
+
+fs.copyFile(file, source, (err) => {
+  if (err) throw err;
+});
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(source);
 const middlewares = jsonServer.defaults();
-const { v4: uuidv4 } = require("uuid");
+// const { v4: uuidv4 } = require("uuid");
 
 //github.com/typicode/json-server/issues/366#issuecomment-439008812
 https: router.db._.mixin({
